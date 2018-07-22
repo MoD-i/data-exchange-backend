c663d74ea6c5e2905580b8f14e00dbb3204e1010
Toran Sahu <toran.sahu@yahoo.com>
2018-07-22 14:41:01 +0000

    [new] only model

---
+++ src/dep2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Dep2Config(AppConfig):
+    name = 'dep2'
---
+++ src/dep2/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Scheme(models.Model):
+    
+    FEMALE = 'F'
+    MALE = 'M'
+    TRANS = 'T'
+    GENDER_CHOICES = ((FEMALE, 'Female'), (MALE, 'Male'), (TRANS, 'Transgender'))
+
+    aadhaar_number = models.CharField(max_length=15, unique=True, primary_key=True)
+    beneficiary_name = models.CharField(max_length = 50, null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=1, null=True, blank=True)
+    member_age =  models.IntegerField(null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    dist_name = models.CharField(max_length=50, null=True, blank=True)
+    scheme_name = models.CharField(max_length=100, null=True, blank=True)
