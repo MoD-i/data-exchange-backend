14e0dd3aeea432f897f1994a7270765a3bc86bac
Toran Sahu <toran.sahu@yahoo.com>
2018-07-22 22:24:16 +0530

    [TODO]

--- src/dep1/views.py
+++ src/dep1/views.py
@@ -3,7 +3,8 @@ from .models import Scheme
 from .serializers import SchemeSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from utils.multichain_api import api, publish_stream
+from utils.multichain_api import api, publish_stream, get_tx_data
+from utils import hex_to_json
 from uuid import uuid4
 
 # Create your views here.
@@ -12,15 +13,17 @@ class SchemeViewSet(viewsets.ModelViewSet):
     queryset = Scheme.objects.all()
     serializer_class = SchemeSerializer
 
+
 @api_view(['POST'])
 def make_request(request):
     data = request.data
     stream = 'scheme'
-    _ticket_no = 1  #TODO: get it from Ticket Model using prefetc_id() 
+    _ticket_no = 1  #TODO: get it from Ticket Model using prefetch_id() 
     _from = 'dep1'
     _to = 'dep2'  #TODO: get it from request.data
     key = f'{_from}-{_to}-{_ticket_no}'
-    txid = publish_stream(stream, key, data)
+    data_to_publish = ''  #TODO: get it from request.data
+    txid = publish_stream(stream, key, data_to_publish, data_format='json')
     
     if txid:
         # TODO: define notify()
@@ -28,9 +31,11 @@ def make_request(request):
     else:
         pass  # inform user about failure
 
+
 @api_view(['POST'])
 def load_data(request):
     data = request.data
-
-
-    
+    txid = ''  # TODO: get it from notification/table 
+    tx_data = get_tx_data(txid)
+    json_data = hex_to_json(tx_data)
+    # for data in json_data:
