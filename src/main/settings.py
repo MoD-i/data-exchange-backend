3583b23382d4c2f8d4168f438dd54121fdf74a34
Toran Sahu <toran.sahu@yahoo.com>
2018-07-23 12:07:30 +0530

    added CSRF; allowed all

--- src/main/settings.py
+++ src/main/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'dep1',
     'dep2',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +133,8 @@ STATIC_URL = '/static/'
 
 
 # DATABASE_OPTIONS = dict(charset="utf8")
+
+##
+# CSRF
+##
+CORS_ORIGIN_ALLOW_ALL = True  
